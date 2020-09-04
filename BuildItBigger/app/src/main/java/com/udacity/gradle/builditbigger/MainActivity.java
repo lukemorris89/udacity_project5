34617899d45f6777a6a7e095d11432c248360096
Luke Morris <luke.morris@bondhealth.co.uk>
2020-09-04 09:35:30 +0100

    GCE added. App now communicates with endpoint and sends a toast to MainActivity.

--- BuildItBigger/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ BuildItBigger/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,15 +1,10 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
-
-import com.example.androidjokelib.JokeActivity;
-import com.example.jokelib.Jokes;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -44,11 +39,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Jokes jokes = new Jokes();
-        String joke = jokes.getJoke();
-        Intent intent = new Intent(this, JokeActivity.class);
-        intent.putExtra(Jokes.INTENT_KEY_JOKE, joke);
-        startActivity(intent);
+        new EndpointAsyncTask().execute(this);
     }
 
 
--- BuildItBigger/backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ BuildItBigger/backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger.backend;
 
+import com.example.jokelib.Jokes;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
@@ -24,6 +25,14 @@ public class MyEndpoint {
         MyBean response = new MyBean();
         response.setData("Hi, " + name);
 
+        return response;
+    }
+
+    @ApiMethod(name = "tellJoke")
+    public MyBean tellJoke(){
+        MyBean response = new MyBean();
+        Jokes jokes = new Jokes();
+        response.setData(jokes.getJoke());
         return response;
     }
 
